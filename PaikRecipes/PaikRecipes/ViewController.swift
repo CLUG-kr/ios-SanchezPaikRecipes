b87edc5247e0e3ea3a54a8ce3d7c846a848ed513
sspog <sspog.lim@gmail.com>
2019-07-18 21:58:49 +0900

    Start 'PaikRecipes' Xcode Project

---
+++ PaikRecipes/PaikRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaikRecipes
+//
+//  Created by Tars on 7/18/19.
+//  Copyright © 2019 sspog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
